85f7ceed5a93aad6b79811878673b8aeadd64ad3
Sıla Pala <92298282+silapala@users.noreply.github.com>
2025-04-24 15:43:30 +0300

    git test edildi

--- app/src/main/java/com/example/myapplication/Giris.kt
+++ app/src/main/java/com/example/myapplication/Giris.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Giris : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_giris)
+        lateinit var button: Button
     }
 }
